2914637914a3b1bb9841f169c16e77720a2d7ff1
Anastasiya <Tsybusova>
2022-05-31 11:18:18 +0300

    Done Task4

--- Task3.py
+++ Task3.py
@@ -9,9 +9,13 @@ class ItemDiscountReport(ItemDiscount):
         self.__name = name
         self.__price = price
 
+    def set_price(self, price):
+        self.__price = price
+
     def get_parent_data(self):
         print(self.__name, self.__price)
 
 
 child = ItemDiscountReport('sale', 10)
+child.set_price(30)
 print(child.get_parent_data())
